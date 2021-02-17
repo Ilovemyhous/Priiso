507c6f8dad826c4f4450c0178d7cd381ae2ad841
Ilovemyhouse <50217071+Ilovemyhous@users.noreply.github.com>
2021-02-17 22:02:55 +0100

    Add files via upload

---
+++ build/fatMounter.d
@@ -0,0 +1,16 @@
+fatMounter.o: /home/tom/Documents/Homebrew/template/source/fatMounter.c \
+ /opt/devkitpro/libogc/include/fat.h \
+ /opt/devkitpro/libogc/include/libfatversion.h \
+ /opt/devkitpro/libogc/include/ogc/disc_io.h \
+ /opt/devkitpro/libogc/include/gctypes.h \
+ /opt/devkitpro/libogc/include/sdcard/wiisd_io.h
+
+/opt/devkitpro/libogc/include/fat.h:
+
+/opt/devkitpro/libogc/include/libfatversion.h:
+
+/opt/devkitpro/libogc/include/ogc/disc_io.h:
+
+/opt/devkitpro/libogc/include/gctypes.h:
+
+/opt/devkitpro/libogc/include/sdcard/wiisd_io.h:
---
+++ include/fatMounter.h
@@ -0,0 +1,17 @@
+#ifndef _FATMOUNTER_H_
+#define _FATMOUNTER_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// Prototypes
+void MountSD(void);
+void UnmountSD(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
